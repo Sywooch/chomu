0f21f7e3dda57973233a24a6b6a6c74273a33b7c
Eugene Fabrikov <eugene.fabrikov@gmail.com>
2015-08-23 21:07:36 +0300

    facebook login logic

--- models/eauth/FacebookOAuth2Service.php
+++ models/eauth/FacebookOAuth2Service.php
@@ -13,7 +13,11 @@ class FacebookOAuth2Service extends \nodge\eauth\services\FacebookOAuth2Service
 {
 
 	protected $scopes = array(
-		'email', 'first_name', 'last_name'
+		self::SCOPE_EMAIL,
+		self::SCOPE_USER_BIRTHDAY,
+		self::SCOPE_USER_HOMETOWN,
+		self::SCOPE_USER_LOCATION,
+		self::SCOPE_USER_PHOTOS,
 	);
 
 	/**
