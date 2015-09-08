0e18e47f90439df69fbadf2364a6d591e3ec5fe5
ndrozd <iam@zemel.info>
2015-09-08 17:00:26 +0300

    login

--- views/site/login.php
+++ views/site/login.php
@@ -9,13 +9,11 @@ use yii\bootstrap\ActiveForm;
 $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div id="email-login" class="popup popup_email-signup " style="">
+<div id="email-signup" class="popup popup_email-signup " style="">
     <div class="popup-inside"> 
         <button class="popup__close"></button>
-        <div class="site-login">
+        <div class="site-login tcenter">
             <div class="nd-popup-head"><?= Html::encode($this->title) ?></div>
-
-            <p>Please fill out the following fields to login:</p>
 
             <?php $form = ActiveForm::begin([
                 'id' => 'login-form',
@@ -29,27 +27,21 @@ $this->params['breadcrumbs'][] = $this->title;
             <?= $form->field($model, 'email') ?>
 
             <?= $form->field($model, 'password')->passwordInput() ?>
+            <div class="pass-reminder"><a href="/reset.html">Забули пароль?</a></div>
 
-            <?= $form->field($model, 'rememberMe', [
+            <!--<?= $form->field($model, 'rememberMe', [
                 'template' => "<div class=\"col-lg-offset-1 col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
-            ])->checkbox() ?>
+            ])->checkbox() ?>-->
 
-            <div class="form-group">
-                <div class="col-lg-offset-1 col-lg-11">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                </div>
-            </div>
+            <?= Html::submitButton('Login', ['class' => 'popup__yellow-btn', 'name' => 'login-button']) ?>
+
 
             <?php ActiveForm::end(); ?>
 
-            <div class="pass-reminder"><a href="/reset.html">Забули пароль?</a></div>
-
-
-
-            <div class="col-lg-offset-1" style="color:#999;">
+           <!--  <div class="col-lg-offset-1" style="color:#999;">
                 You may login with <strong>admin/admin</strong> or <strong>demo/demo</strong>.<br>
                 To modify the username/password, please check out the code <code>app\models\User::$users</code>.
-            </div>
+            </div> -->
         </div>
     </div>
 </div>
