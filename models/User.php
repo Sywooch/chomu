8ef7d8d53aa66ff96dbc704b8fc0fbd2226cbe53
aivavic <viktor85a@gmail.com>
2015-09-08 07:44:16 +0300

    test email

--- models/User.php
+++ models/User.php
@@ -278,9 +278,11 @@ class User extends \yii\db\ActiveRecord implements IdentityInterface
     /**
      * @inheritdoc
      */
+
     public static function findIdentityByAccessToken($token, $type = null)
     {
-        throw new NotSupportedException('findIdentityByAccessToken is not implemented.');
+        return static::findOne(['email_confirm_token' => $token]);
+//        throw new NotSupportedException('findIdentityByAccessToken is not implemented.');
     }
 
     /**
