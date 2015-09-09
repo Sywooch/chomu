f9330591af74bbbd0b14e07ea929b7cb18e39705
aivavic <viktor85a@gmail.com>
2015-09-09 17:26:46 +0300

    fix forms

--- views/site/resetPassword.php
+++ views/site/resetPassword.php
@@ -1,24 +1,30 @@
 <?php
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
+
 /* @var $this yii\web\View */
 /* @var $form yii\bootstrap\ActiveForm */
 /* @var $model \frontend\models\ResetPasswordForm */
-$this->title = 'Reset password';
+$this->title = 'Скинути пароль';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-reset-password">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please choose your new password:</p>
+    <p>Будь-ласка, введіть новий пароль:</p>
 
     <div class="row">
         <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'reset-password-form']); ?>
-                <?= $form->field($model, 'password')->passwordInput() ?>
-                <div class="form-group">
-                    <?= Html::submitButton('Save', ['class' => 'btn btn-primary']) ?>
-                </div>
+            <?php $form = ActiveForm::begin([
+                'id' => 'reset-password-form',
+                'options' => [
+                    'class' => 'cssFormClassName'
+                ]
+            ]); ?>
+            <?= $form->field($model, 'password')->passwordInput() ?>
+            <div class="form-group">
+                <?= Html::submitButton('Save', ['class' => 'btn btn-primary']) ?>
+            </div>
             <?php ActiveForm::end(); ?>
         </div>
     </div>
