e6d44b611f482ca818b69fcb35de6e8cdcacbcb5
aivavic <viktor85a@gmail.com>
2015-09-29 13:52:52 +0300

    news

--- views/site/news.php
+++ views/site/news.php
@@ -153,12 +153,12 @@ $this->title = isset($new) && $new !== null ? $new->title : 'Новини';
         background-position: -128px 8px;
     }
     li.fb span.icon-inner-text:hover:before{
-        background-position: 0px -60px;
+        background-position: 0px -59px;
     }
     li.ok span.icon-inner-text:hover:before{
-        background-position: -64px -60px;
+        background-position: -64px -59px;
     }
     li.vk span.icon-inner-text:hover:before{
-        background-position: -128px -60px;
+        background-position: -128px -59px;
     }
 </style>
